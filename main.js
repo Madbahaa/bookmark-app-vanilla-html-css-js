8669b98d26dd9c81d0c4af2749676917febff38e
Madbahaa <madbahaa@gmail.com>
2017-12-01 15:12:46 +0000

    fixing last buggs

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ function saveBookMark(e){
     e.preventDefault();
 }
 
-function deleteBookmark(url){
     function deleteBookmark(url){
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         for(var i =0;i < bookmarks.length;i++){
@@ -37,7 +36,7 @@ function deleteBookmark(url){
         fetchBookmarks();
       }
 
-}
+
 
 function fetchBookmarks(){
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
@@ -48,7 +47,7 @@ function fetchBookmarks(){
       var url = bookmarks[i].url;
       bookmarksResults.innerHTML += '<blockquote>'+
                                     '<h4><em>' +name+ '</em></h4>'+
-                                    '<a href="'+url+'"><input  class="button-outline" type="submit" value="visit"></a>'+ 
+                                    '<a href="'+url+'"><input  class="button-outline" type="submit" value="visit"></a> '+ 
                                     '<input onclick="deleteBookmark(\''+url+'\')" class="button-primary" type="submit" value="Delete">'+
                                     '</blockquote>';
     }
