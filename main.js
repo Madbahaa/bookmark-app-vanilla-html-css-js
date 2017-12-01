5cae41fc5cd2caf6c4a6e07685c5e5e69d4b10af
Madbahaa <madbahaa@gmail.com>
2017-12-01 14:57:45 +0000

    delete bookmark function

--- main.js
+++ main.js
@@ -21,5 +21,20 @@ function saveBookMark(e){
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
     document.getElementById('myForm').reset();
+    
     e.preventDefault();
+}
+
+function deleteBookmark(url){
+    function deleteBookmark(url){
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        for(var i =0;i < bookmarks.length;i++){
+          if(bookmarks[i].url == url){
+            bookmarks.splice(i, 1);
+          }
+        }
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+      
+      }
+
 }
