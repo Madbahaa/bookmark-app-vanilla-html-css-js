abf38597707e351d201b4213546b9463c52054a6
Madbahaa <madbahaa@gmail.com>
2017-12-01 15:08:09 +0000

    fething bookmark data an output it inside the body

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function saveBookMark(e){
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
     document.getElementById('myForm').reset();
-    
+    fetchBookmarks();
     e.preventDefault();
 }
 
@@ -34,7 +34,22 @@ function deleteBookmark(url){
           }
         }
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-      
+        fetchBookmarks();
       }
 
 }
+
+function fetchBookmarks(){
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarksResults = document.getElementById('bookmarksOutput');
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++){
+      var name = bookmarks[i].name;
+      var url = bookmarks[i].url;
+      bookmarksResults.innerHTML += '<blockquote>'+
+                                    '<h4><em>' +name+ '</em></h4>'+
+                                    '<a href="'+url+'"><input  class="button-outline" type="submit" value="visit"></a>'+ 
+                                    '<input onclick="deleteBookmark(\''+url+'\')" class="button-primary" type="submit" value="Delete">'+
+                                    '</blockquote>';
+    }
+  }
